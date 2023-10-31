8ab624356179edc7bb48e702033127d0cce3dff1
xtreme <anastaai94@gmail.com>
2023-11-01 00:03:50 +0100

    added 10-add.py

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add(a, b):
+    if not (isinstance(a, int) and isinstance(b, int)):
+        return
+    return a + b
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    if not isinstance(number, int):
+        return
     print("{}".format(str(number)[-1]), end='')
     return str(number)[-1]
