8fb2fd4c0e410a06920f8d84af3ca0a23b52d954
xtreme <anastaai94@gmail.com>
2023-10-31 21:29:15 +0100

    added 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(string):
+    buff = ""
+    for s in string:
+        if ord(s) >= 97 and ord(s) <= 122:
+            buff += chr(ord(s) - 32)
+        else:
+            buff += s
+    print(buff)
