2d6c877325b5d2902ea901e3a3de6c92e5891541
xtreme <anastaai94@gmail.com>
2023-11-01 00:13:53 +0100

    fixed 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if not isinstance(number, int):
-        return ''
+        exit()
     print("{}".format(str(number)[-1]), end='')
     return str(number)[-1]
