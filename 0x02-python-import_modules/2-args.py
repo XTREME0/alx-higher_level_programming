c7d13a9d1ff51f1159fea176301d5a805462ab15
xtreme <anastaai94@gmail.com>
2023-11-02 15:50:56 +0100

    added 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv)
+    if l == 2:
+        print("{} argument.".format(1))
+    else:
+        print("{} arguments.".format(l-1))
+    if l > 1:
+        for i in range(1, l):
+            print("{}: {}".format(i, sys.argv[i]))
