251601c4cd47b75a48f0230f6752efbe01fae8b3
xtreme <anastaai94@gmail.com>
2023-11-02 16:43:43 +0100

    fixed 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     l = len(sys.argv)
     if l == 2:
         print("{} argument:".format(1))
+    elif l == 1:
+        print("{} arguments.".format(0))
     else:
         print("{} arguments:".format(l-1))
     if l > 1:
