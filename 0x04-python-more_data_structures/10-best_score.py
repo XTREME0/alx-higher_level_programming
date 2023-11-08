a460423106abaeb27e46a5fc7391f53c1608a8f8
xtreme <anastaai94@gmail.com>
2023-11-08 22:04:53 +0100

    added 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    maxx = max(a_dictionary.values())
+    maxx = max(list(a_dictionary.values()))
     for key, value in a_dictionary.items():
         if value == maxx:
             return key
