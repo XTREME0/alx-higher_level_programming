2b1a7aa724029a404486382dce16529012069837
xtreme <anastaai94@gmail.com>
2023-11-08 18:36:32 +0100

    added 2-uniq_add.py

--- 0x04-python-more_data_structures/2-main.py
+++ 0x04-python-more_data_structures/2-main.py
@@ -4,3 +4,12 @@ uniq_add = __import__('2-uniq_add').uniq_add
 my_list = [1, 2, 3, 1, 4, 2, 5]
 result = uniq_add(my_list)
 print("Result: {:d}".format(result))
+my_list = [-1, 2, -1, 1, 7, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
+my_list = [0, 2, 3, 1, 0, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
+my_list = None
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if not my_list:
+        return 0
     result = 0
     lst = my_list
     lst.sort()
@@ -8,4 +10,3 @@ def uniq_add(my_list=[]):
             result += lst[i]
     result += lst[i + 1]
     return result
-
