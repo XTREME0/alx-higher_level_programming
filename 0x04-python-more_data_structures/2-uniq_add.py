7b9bf3d65ecda7fd15712c4e3c6703244cb32cf9
xtreme <anastaai94@gmail.com>
2023-11-08 18:30:51 +0100

    added 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    lst = my_list
+    lst.sort()
+    for i in range(len(lst) - 1):
+        if lst[i] != lst[i + 1]:
+            result += lst[i]
+    result += lst[i + 1]
+    return result
+
