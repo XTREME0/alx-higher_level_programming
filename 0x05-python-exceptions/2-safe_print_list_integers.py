877da9789b3cda3a351ead3c40823fc4b187ebb2
xtreme <anastaai94@gmail.com>
2023-11-21 00:38:05 +0100

    added 3-safe_print_division.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    j = 0
     while x > 0:
         try:
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
             i += 1
-            j += 1
             x -= 1
         except ValueError:
             i += 1
-            continue
-        except IndexError:
-            break
+            x -= 1
     print("")
-    return j
+    return i
