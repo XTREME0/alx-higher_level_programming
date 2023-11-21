0085abccc1c97e03ce42bd60642bac114ce51576
xtreme <anastaai94@gmail.com>
2023-11-21 14:47:02 +0100

    added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" define a square class """
+
+
+class Square:
+    """ square class """
+
+    __size = None
+
+    def __init__(self, size=0):
+        s = 0
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
