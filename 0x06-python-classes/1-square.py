be2cd2fbbb9414a7275cf6b529124d93454cc9cf
xtreme <anastaai94@gmail.com>
2023-11-21 14:25:49 +0100

    added 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ define a square class """
 
+
 class Square:
     """ square class """
 
     __size = None
+
     def __init__(self, size=None):
         self.__size = size
