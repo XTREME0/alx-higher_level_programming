e434bbf5b8a77813e9e7963cc58f40ae9aeb9bb7
xtreme <anastaai94@gmail.com>
2023-12-05 22:13:27 +0100

    first commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+
 """ read_file function """
 
 def read_file(filename=""):
-""" function to read a file and print it """
+    """ 
+    function to read a file and print it 
+    Args:
+        filename: filename duuh
+    """
+
     with open(filename, 'r') as file:
         for l in file:
             print(l, end="")
