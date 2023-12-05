6dca6d0d48463582aab111b6b5d48beba49ffeb2
xtreme <anastaai94@gmail.com>
2023-12-05 22:26:25 +0100

    added 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,5 +10,5 @@ def write_file(filename="", text=""):
         text: txt to write in file
     """
 
-    with open(fillename, mode="w", encoding="UTF-8") as file:
+    with open(filename, 'w', encoding="UTF-8") as file:
         return (f.write(text))
