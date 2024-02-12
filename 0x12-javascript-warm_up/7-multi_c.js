1998149e94140ff4f3835622c113f876ac5702e4
xtreme <anastaai94@gmail.com>
2024-02-12 17:36:35 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,6 +6,6 @@ if (isNaN(parseInt(argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(argv[2]); i++) {
-    console.log('x');
+    console.log('C is fun');
   }
 }
