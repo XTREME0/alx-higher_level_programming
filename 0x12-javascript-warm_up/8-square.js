8c96332cfe270051bc3d3444887194a7682833b8
xtreme <anastaai94@gmail.com>
2024-02-12 18:18:48 +0100

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const num = parseInt(argv[2]);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+}
