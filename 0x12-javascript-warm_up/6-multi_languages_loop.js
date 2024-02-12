5216191a4281d5b2bffeb900129848081b7e78d2
xtreme <anastaai94@gmail.com>
2024-02-12 17:26:52 +0100

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const array = ['C', 'Python', 'JavaScript'];
+const adj = ['fun', 'cool', 'amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(array[i] + ' is ' + adj[i]);
+}
