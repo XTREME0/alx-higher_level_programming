11592760ba5b558063a0a8d7be47643cacdf16fb
xtreme <anastaai94@gmail.com>
2024-02-13 17:46:04 +0100

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+}
+module.exports = Rectangle;
