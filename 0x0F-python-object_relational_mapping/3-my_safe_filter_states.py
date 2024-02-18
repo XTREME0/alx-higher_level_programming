6b50b14d0be433c3b1bb2e458cd5de18c2f61faa
xtreme <anastaai94@gmail.com>
2024-02-18 22:37:11 +0100

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" print states from states table and search based on first letter """
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    host = "localhost"
+    port = 3306
+    if argc < 4:
+        exit()
+    username = argv[0]
+    password = argv[1]
+    db_name = argv[2]
+    search = argv[3]
+
+    connection = MySQLdb.connect(host=host, port=port,
+                            user=username, password=password, database=db_name)
+    crs = connection.cursor()
+    crs.execute("SELECT * FROM states WHERE name=%s\
+            ORDER BY 'id';", search)
+    r = crs.fetchall()
+    for row in r:
+        print(row)
