8f23df49692147266db7b15a8d3dc9adcfc53151
xtreme <anastaai94@gmail.com>
2024-02-19 00:02:28 +0100

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" print states from states table and search based on first letter """
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    host = "localhost"
+    port = 3306
+    username = argv[0]
+    password = argv[1]
+    db_name = argv[2]
+    name = argv[3]
+
+    connection = MySQLdb.connect(host=host, port=port,
+                            user=username, password=password, database=db_name)
+    crs = connection.cursor()
+    crs.execute("SELECT cities.name FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id", name)
+    r = crs.fetchall()
+    for row in r:
+        print(row)
