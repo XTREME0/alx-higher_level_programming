b96c176dca307126185c9771e2e7282f8e7119fa
xtreme <anastaai94@gmail.com>
2024-02-23 21:02:41 +0100

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" print states from states using sqlalchemy """
+
+if __name__ == "__main__":
+
+    import sys
+    from models_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    host = "localhost"
+    port = 3306
+    if argc < 4:
+        exit()
+    username = argv[0]
+    password = argv[1]
+    db_name = argv[2]
+    name = argv[3]
+
+    connection = f'mysql+mysqldb://{username}:{password}@{host}:{port}/{db_name}'
+    crs = create_engine(connection)
+    Base.metadata.create_all(crs)
+    Session = sessionmake(bind=crs)
+    s = session()
+    f = s.query(State).first()
+    if f:
+        print("Nothing")
+    else:
+        print(f'{f.id}: {f.name}')
