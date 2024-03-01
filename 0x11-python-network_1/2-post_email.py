7cbcfdbbe4ae2dac1135b4a8627fb454f9acde4d
xtreme <anastaai94@gmail.com>
2024-03-01 18:59:02 +0100

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,8 +3,6 @@
 if __name__ == "__main__":
     import urllib.request
     import sys
-    if len(sys.argv) > 3:
-        exit(-1)
     d = urllib.parse.urlencode('email': sys.argv[2]).encode("utf-8")
     r = urllib.request.Request(sys.argv[1], d)
     with urllib.request.urlopen(r) as resp:
