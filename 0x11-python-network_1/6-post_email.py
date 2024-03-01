0a549ad8cc4d7e44bbf71966da559e5c87fef5fd
xtreme <anastaai94@gmail.com>
2024-03-01 22:50:37 +0100

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" send mail """
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
