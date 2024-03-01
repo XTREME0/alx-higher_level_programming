645eb9344ce75fa31ab9be922a1b10b701b9b45e
xtreme <anastaai94@gmail.com>
2024-03-01 23:27:26 +0100

    task 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     import sys
     import requests
-    r = requests.get(f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits')
+    r = requests.get(f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits')
     c = 10
     for commit in r.json():
         print(f'{commit.get("sha")}: {commit.get("commit").get("author").get("name")}')
