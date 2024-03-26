712e0f262d9aa90cb284fd5e8af23d58913c85ed
xtreme <anastaai94@gmail.com>
2024-03-26 15:48:48 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const fs = require('fs');
+
+try {
+	const contant = fs.readFileSync(argv[2], 'utf8');
+	console.log(contant);
+} catch (error) {
+	console.log(error);
+}
