b83eaf58de50083e1d6340a6e09557e03c514f2d
xtreme <anastaai94@gmail.com>
2024-03-26 17:46:13 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/tst.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const r = require('request');
+const link = argv[2];
+count = 0;
+r.get(link, (err, resp, pg) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  pg = JSON.parse(pg);
+  for (film in pg.results) {
+    if (pg.results[film].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count += 1;
+    }
+  }
+    console.log(count);
+});
